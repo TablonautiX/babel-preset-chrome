b768afa388987fab7441c19d5fe525f7e7687644
David Steinkopff <d.steinkopff@tablonautix.com>
2015-11-20 13:38:29 +0100

    fiat lux

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # babel-preset-chrome
+
+> Babel preset for chrome for version 46+.
+
+## Install
+
+```sh
+$ npm install --save-dev babel-preset-chrome
+```
+
+## Usage
+
+### Via `.babelrc` (Recommended)
+
+**.babelrc**
+
+```json
+{
+  "presets": ["babel-preset-chrome"]
+}
+```
+
+### Via CLI
+
+```sh
+$ babel script.js --presets babel-preset-chrome
+```
+
+### Via Node API
+
+```javascript
+require("babel-core").transform("code", {
+  presets: ["babel-preset-chrome"]
+});
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: [
+        require("babel-plugin-transform-es2015-function-name"),
+        require("babel-plugin-transform-es2015-for-of"),
+        require("babel-plugin-transform-es2015-sticky-regex"),
+        require("babel-plugin-transform-es2015-unicode-regex"),
+        require("babel-plugin-check-es2015-constants"),
+        require("babel-plugin-transform-es2015-parameters"),
+        require("babel-plugin-transform-es2015-destructuring"),
+        require("babel-plugin-transform-es2015-block-scoping"),
+        require("babel-plugin-transform-es2015-typeof-symbol"),
+        require("babel-plugin-transform-es2015-modules-commonjs"),
+        [require("babel-plugin-transform-regenerator"), {async: false, asyncGenerators: false}]
+    ]
+};

0b2d62ea0d6237cbb5ec2061082c6d506b6ffb15
David Steinkopff <d.steinkopff@tablonautix.com>
2015-11-20 14:32:36 +0100

    problem with babel-plugin-transform-regenerator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         require("babel-plugin-transform-es2015-function-name"),
+        require("babel-plugin-transform-es2015-arrow-functions"),
         require("babel-plugin-transform-es2015-for-of"),
         require("babel-plugin-transform-es2015-sticky-regex"),
         require("babel-plugin-transform-es2015-unicode-regex"),
